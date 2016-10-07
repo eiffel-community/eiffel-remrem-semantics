727d309054478977fce38c01b62e39e5f68f08f8
Vali (Vasile Baluta) <vasile.baluta@ericsson.com>
2016-10-07 09:26:11 +0200

    update Spring version and Event version

--- src/test/java/com/ericsson/eiffel/remrem/semantics/ServiceTest.java
+++ src/test/java/com/ericsson/eiffel/remrem/semantics/ServiceTest.java
@@ -1,13 +1,23 @@
 package com.ericsson.eiffel.remrem.semantics;
 
+import com.ericsson.eiffel.remrem.semantics.events.Event;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class ServiceTest {
 
@@ -15,8 +25,20 @@ public class ServiceTest {
     private String ARTIFACT_PUBLISHED = "eiffelartifactpublished";
 
     JsonParser parser = new JsonParser();
+    
+    @InjectMocks
     SemanticsService service = new SemanticsService();
 
+    
+    private Event event;
+        
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        event = mock(Event.class);
+        when(event.getCurrentVersion()).thenReturn("0.1.0");
+    }
+    
     private void testGenerateMsg(String msgType, String fileName) {
 
         try {
