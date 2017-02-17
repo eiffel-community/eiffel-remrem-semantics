0e5d1151fc93e117090196fe1505ad90e7256834
UMADEVI KAPU <kapu.umadevi@tcs.com>
2017-02-17 15:02:36 +0530

    Changed return statements in Event class

--- src/main/java/com/ericsson/eiffel/semantics/events/Event.java
+++ src/main/java/com/ericsson/eiffel/semantics/events/Event.java
@@ -2,13 +2,16 @@ package com.ericsson.eiffel.semantics.events;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ericsson.eiffel.remrem.semantics.RoutingKeyTypes;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public abstract class Event {
     public transient Meta meta;
-    public static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Event.class);
+    public static final Logger log = LoggerFactory.getLogger(Event.class);
     public void generateMeta(String msgType, JsonObject msgNodes) {
         meta = new Gson().fromJson(msgNodes.get("meta"), Meta.class);
         meta.setType(Meta.Type.fromValue(msgType));
@@ -20,22 +23,20 @@ public abstract class Event {
     public abstract void setMeta(Meta meta);
 
     public static String getFamilyRoutingKey(String eventType) {
-        RoutingKeyTypes eventtype = null;
         try {
-            eventtype = RoutingKeyTypes.valueOf(eventType);
+           return RoutingKeyTypes.valueOf(eventType).getFamily();
         } catch (Exception e) {
             log.error("Unknown event type requested: " + eventType);
         }
-        return eventtype.getFamily();
+        return null;
     }
 
     public static String getTypeRoutingKey(String eventType) {
-        RoutingKeyTypes eventtype = null;
         try {
-            eventtype = RoutingKeyTypes.valueOf(eventType);
+            return RoutingKeyTypes.valueOf(eventType).getType();
         } catch (Exception e) {
             log.error("Unknown event type requested: " + eventType);
         }
-        return eventtype.getType();
+        return null;
     }
 }
