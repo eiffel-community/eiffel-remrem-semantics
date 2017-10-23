8b0c408fbab22affece7297c5912ae7c902cff1e
xdurvak <prasad.vakapalli@tcs.com>
2017-10-23 15:30:44 +0530

    Update build.gradle and changelog.md files (#53)

--- src/main/java/com/ericsson/eiffel/remrem/semantics/SemanticsService.java
+++ src/main/java/com/ericsson/eiffel/remrem/semantics/SemanticsService.java
@@ -14,7 +14,6 @@
 */
 package com.ericsson.eiffel.remrem.semantics;
 
-
 import static com.ericsson.eiffel.remrem.semantics.EiffelEventType.ACTIVITY_CANCELED;
 import static com.ericsson.eiffel.remrem.semantics.EiffelEventType.ACTIVITY_FINISHED;
 import static com.ericsson.eiffel.remrem.semantics.EiffelEventType.ACTIVITY_STARTED;
@@ -80,7 +79,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 @Named("eiffel-semantics")
-public class SemanticsService implements MsgService{
+public class SemanticsService implements MsgService {
     private static final String ERROR = "error";
     private static final String SUPPORTED_EVENT_TYPES = "SUPPORTED_EVENT_TYPES";
     private static final String RESULT = "result";
@@ -97,16 +96,15 @@ public class SemanticsService implements MsgService{
     public static final Logger log = LoggerFactory.getLogger(SemanticsService.class);
 
     private static Gson gson = new Gson();
-    private static Map<EiffelEventType, Class<? extends Event>> eventTypes=SemanticsService.eventType();
-    
-    public SemanticsService(){
+    private static Map<EiffelEventType, Class<? extends Event>> eventTypes = SemanticsService.eventType();
+
+    public SemanticsService() {
         for (final EiffelEventType msg : EiffelEventType.values()) {
             supportedEventTypes.add(msg.getEventName());
         }
     }
-    
-    public static Map<EiffelEventType, Class<? extends Event>> eventType()
-    {
+
+    public static Map<EiffelEventType, Class<? extends Event>> eventType() {
         eventTypes = new HashMap<>();
         eventTypes.put(ARTIFACT_PUBLISHED, EiffelArtifactPublishedEvent.class);
         eventTypes.put(ACTIVITY_FINISHED, EiffelActivityFinishedEvent.class);
@@ -130,91 +128,96 @@ public class SemanticsService implements MsgService{
         eventTypes.put(TESTSUITE_STARTED, EiffelTestSuiteStartedEvent.class);
         eventTypes.put(ISSUE_VERIFIED, EiffelIssueVerifiedEvent.class);
         eventTypes.put(ARTIFACT_REUSED, EiffelArtifactReusedEvent.class);
-       return eventTypes; 
+        return eventTypes;
     }
 
     @Override
-    public String generateMsg(String msgType, JsonObject bodyJson){
+    public String generateMsg(String msgType, JsonObject bodyJson) {
         EiffelEventType eiffelType = EiffelEventType.fromString(msgType);
         if (eiffelType == null) {
             log.error("Unknown message type requested: " + msgType);
-            return createErrorResponse(msgType,supportedEventTypes);
+            return createErrorResponse(msgType, supportedEventTypes);
         }
         Class<? extends Event> eventType = eventTypes.get(eiffelType);
 
         JsonObject msgNodes = bodyJson.get(MSG_PARAMS).getAsJsonObject();
         JsonObject eventNodes = bodyJson.get(EVENT_PARAMS).getAsJsonObject();
 
-        //Compare the input JSON EventType with query parameter(-t) and also check type exist or not,
-        //if input JSON EventType is missing adding query parameter as Type. 
+        // Compare the input JSON EventType with query parameter(-t) and also
+        // check type exist or not,
+        // if input JSON EventType is missing adding query parameter as Type.
         String inputEventType = getInputEventType(bodyJson);
-        if(inputEventType ==  null || inputEventType.isEmpty()){
-            bodyJson.get(MSG_PARAMS).getAsJsonObject().get(META).getAsJsonObject().addProperty(TYPE, eiffelType.getEventName());
-        }else if(!(inputEventType.equals(eiffelType.getEventName()))){
+        if (inputEventType == null || inputEventType.isEmpty()) {
+            bodyJson.get(MSG_PARAMS).getAsJsonObject().get(META).getAsJsonObject().addProperty(TYPE,
+                    eiffelType.getEventName());
+        } else if (!(inputEventType.equals(eiffelType.getEventName()))) {
             log.error("check the input json message type : " + inputEventType);
-            return createErrorResponse(eiffelType.getEventName(),"Mismatch of eventype in request query parameter with property 'type' in the input json message");
+            return createErrorResponse(eiffelType.getEventName(),
+                    "Mismatch of eventype in request query parameter with property 'type' in the input json message");
         }
 
         Event event = eventCreation(eventType, msgNodes, eventNodes);
 
-       String result = gson.toJson(event);
+        String result = gson.toJson(event);
         try {
             outputValidate(eiffelType, result);
         } catch (EiffelValidationException e) {
-            log.error("Could not validate message. Reason:" +  e.getMessage() +"\nCause: " + e.getCause().toString());
+            log.error("Could not validate message. Reason:" + e.getMessage() + "\nCause: " + e.getCause().toString());
             return createErrorResponse(e.getMessage(), e.getCause().toString());
         }
         return result;
     }
-    private static Event eventCreation(Class<? extends Event> eventType, JsonObject msgNodes,
-            JsonObject eventNodes) {
+
+    private static Event eventCreation(Class<? extends Event> eventType, JsonObject msgNodes, JsonObject eventNodes) {
         eventNodes.add("meta", msgNodes.get("meta"));
         Event event = createEvent(eventNodes, eventType);
         event.setMeta(event.generateMeta(event.getMeta()));
         return event;
     }
-    
+
     private static Event createEvent(JsonObject eventNodes, Class<? extends Event> eventType) {
         return gson.fromJson(eventNodes, eventType);
     }
 
-    private String createErrorResponse(final String message, final String cause){
+    private String createErrorResponse(final String message, final String cause) {
         JsonObject errorResponse = new JsonObject();
         errorResponse.addProperty(MESSAGE, message);
         errorResponse.addProperty(CAUSE, cause.replace("\n", ""));
         return errorResponse.toString();
     }
-    private String createErrorResponse(final String message, final ArrayList<String> supportedEventTypes){
+
+    private String createErrorResponse(final String message, final ArrayList<String> supportedEventTypes) {
         JsonObject errorResponse = new JsonObject();
         errorResponse.addProperty(RESULT, ERROR);
-        errorResponse.addProperty(MESSAGE, UNKNOWN_EVENT_TYPE_REQUESTED+"  -  " + message);
-        errorResponse.addProperty(SUPPORTED_EVENT_TYPES,supportedEventTypes.toString());
+        errorResponse.addProperty(MESSAGE, UNKNOWN_EVENT_TYPE_REQUESTED + "  -  " + message);
+        errorResponse.addProperty(SUPPORTED_EVENT_TYPES, supportedEventTypes.toString());
         return errorResponse.toString();
     }
+
     private void outputValidate(EiffelEventType eiffelType, String jsonStringInput) throws EiffelValidationException {
         EiffelValidator validator = EiffelOutputValidatorFactory.getEiffelValidator(eiffelType);
         JsonObject jsonObject = new JsonParser().parse(jsonStringInput).getAsJsonObject();
         validator.validate(jsonObject);
-        //custom validations on an event which is not covered in schema.
+        // custom validations on an event which is not covered in schema.
         validator.customValidation(jsonObject);
     }
 
     @Override
     public String getEventId(JsonObject json) {
-        if (json.isJsonObject() && json.getAsJsonObject().has(META) && json.getAsJsonObject()
-                .getAsJsonObject(META).has(ID)) {
-            return json.getAsJsonObject().getAsJsonObject(META)
-                    .get(ID).getAsString();
+        if (json.isJsonObject() && json.getAsJsonObject().has(META)
+                && json.getAsJsonObject().getAsJsonObject(META).has(ID)) {
+            return json.getAsJsonObject().getAsJsonObject(META).get(ID).getAsString();
         }
         return null;
     }
 
     /*
-     * json Input json which we have passed from CLI or Service
-     * return eventType will return from inputJson file
+     * json Input json which we have passed from CLI or Service return eventType
+     * will return from inputJson file
      */
     public String getInputEventType(JsonObject json) {
-        if (json.isJsonObject() && json.get(MSG_PARAMS).getAsJsonObject().has(META) && json.get(MSG_PARAMS).getAsJsonObject().getAsJsonObject(META).has(TYPE)){
+        if (json.isJsonObject() && json.get(MSG_PARAMS).getAsJsonObject().has(META)
+                && json.get(MSG_PARAMS).getAsJsonObject().getAsJsonObject(META).has(TYPE)) {
             return json.get(MSG_PARAMS).getAsJsonObject().getAsJsonObject(META).get(TYPE).getAsString();
         }
         return null;
